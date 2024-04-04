1298172f71db771390452dfc67c72175fa65d7af
WeissTheFallen <118644644+WeissTheFallen@users.noreply.github.com>
2024-04-03 23:18:13 -0400

    Update app.ts
    Solamente le agregue el provider.http

--- app.ts
+++ app.ts
@@ -6,7 +6,14 @@ const flowBienvenida=addKeyword('Hola').addAnswer('Buenas, bienvenido')
 
 const main = async ()=>{
     const provider=createProvider(BaileysProvider)
-
+    
+    provider.initHttpServer(3033)
+    provider.http.server.post('/message', handleCtx( async (bot, res, req)=>{
+        const body=req.body
+        await bot.sendMessage(584121247580, 'Funciona la api si te llega', {})
+        res.end('Mensaje enviado')
+    }))
+    
     await createBot({
         flow: createFlow([flowBienvenida]),
         database : new MemoryDB(),
